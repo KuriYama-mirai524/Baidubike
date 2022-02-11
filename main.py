cc7333292d9a914cc506d581efd9a24f340fd491
KuriYama-Mirai <93362741+daisy524@users.noreply.github.com>
2022-02-11 15:22:19 +0800

    1

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import requests
 
-data = dict(verifyKey='daisy')
-url = 'http://127.0.0.1:8080/verify'
-resp = requests.request('post',url, json=data)
-print(resp.json())
+
+# 取得session
+def get_session():
+    data = dict(verifyKey='daisy')
+    url = 'http://127.0.0.1:8080/'
+    resp = requests.request('post', url + 'verify', json=data)
+    print(resp.json())
+    return resp.json()
+
+
+# 将session和bot的qq号绑定
+def link_start(res):
+    print('尝试连接....')
+    url = 'http://127.0.0.1:8080/'
+    data_session = dict(sessionKey=res['session'], qq=2317387881)
+    resp2 = requests.request('post', url + 'bind', json=data_session)
+    if 'success' in resp2.text:
+        print('连接成功！')
+    if '2' in resp2.text:
+        print('指定的bot不存在')
+
+if __name__ == '__main__':
+    link_start(res=get_session())
+
+
