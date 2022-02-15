6445f5e2ab0e9589397c0828cdc05eb6a61220e0
KuriYama-Mirai <93362741+daisy524@users.noreply.github.com>
2022-02-15 18:10:44 +0800

    Update 测试.py

--- 测试.py
+++ 测试.py
@@ -2,19 +2,23 @@ import sys
 
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
 import os
-# opt = ChromeOptions()
-# opt.add_argument('--headless')
-# opt.add_argument('--no-sandbox')
-# opt.add_argument('--disable-dev-shm-usage')
-#
-# web = webdriver.Chrome(options=opt)
-# web.get('https://www.baidu.com')
-# print(web.page_source)
-p = str(os.path.abspath(sys.argv[0]))
-path = str(p.strip('测试.py')+'config.txt')
-with open(path, 'r') as f:
-    data3 = f.readlines()
-    print(data3[2].split(':')[1])
+js = "window.scrollTo(0,10000)"
+
+
+option = ChromeOptions()
+option.add_argument('--no-sandbox')
+option.add_argument('--disable-dev-shm-usage')
+web = webdriver.Chrome(options=option)
+print('正在截图中...')
+web.set_window_size(1920, 1080)
+web.execute_script(js)
+web.get('https://baike.baidu.com/item/'+'栗山未来')
+img = web.find_element(By.XPATH, '/html/body/div[3]/div[2]').screenshot_as_png
+with open('I:/mirai/cs.png', 'wb+') as f:
+    f.write(img)
+    print('搜索完成')
+
 
 
